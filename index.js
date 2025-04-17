4f76c82d5a3d730b2aacbc56fe9fa30972845542
VlasceanuAndrei <57728913+VlasceanuAndrei@users.noreply.github.com>
2025-04-17 22:23:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,151 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+
+console.log("Folderul proiectului: ", __dirname)
+console.log("Calea fisierului index.js: ", __filename)
+console.log("Folderul curent de lucru: ", process.cwd())
+
+app.set("view engine", "ejs");
+
+obGlobal={
+    obErori:null
+}
+
+
+function initErori(){
+    let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
+    console.log(continut)
+    obGlobal.obErori=JSON.parse(continut)
+    console.log(obGlobal.obErori)
+    
+    obGlobal.obErori.eroare_default.imagine=path.join(obGlobal.obErori.cale_baza, obGlobal.obErori.eroare_default.imagine)
+    for (let eroare of obGlobal.obErori.info_erori){
+        eroare.imagine=path.join(obGlobal.obErori.cale_baza, eroare.imagine)
+    }
+    console.log(obGlobal.obErori)
+
+}
+
+initErori()
+
+function afisareEroare(res, identificator, titlu, text, imagine){
+    let eroare= obGlobal.obErori.info_erori.find(function(elem){ 
+                        return elem.identificator==identificator
+                    });
+    if(eroare){
+        if(eroare.status)
+            res.status(identificator)
+        var titluCustom=titlu || eroare.titlu;
+        var textCustom=text || eroare.text;
+        var imagineCustom=imagine || eroare.imagine;
+
+
+    }
+    else{
+        var err=obGlobal.obErori.eroare_default
+        var titluCustom=titlu || err.titlu;
+        var textCustom=text || err.text;
+        var imagineCustom=imagine || err.imagine;
+
+
+    }
+    res.render("pagini/eroare", { //transmit obiectul locals
+        titlu: titluCustom,
+        text: textCustom,
+        imagine: imagineCustom
+})
+
+}
+
+
+app.use("/resurse", function(req, res, next){
+    let caleFisier = path.join(__dirname, "resurse", req.url);
+    if (fs.existsSync(caleFisier) && fs.lstatSync(caleFisier).isDirectory()) {
+        afisareEroare(res, 403);
+    } else {
+        next();
+    }
+});
+
+app.use("/resurse", express.static(path.join(__dirname, "resurse"), {
+    index: false,
+    fallthrough: true
+}))
+
+
+
+app.get("/favicon.ico", function(req, res){
+    res.sendFile(path.join(__dirname, "resurse/ico/favicon/favicon.ico"));
+})
+
+app.get(["/","/index","/home"], function(req, res){
+    res.render("pagini/index",{ip:req.ip});
+})
+
+
+app.get("/index/a", function(req, res){
+    res.render("pagini/index");
+})
+
+
+app.get("/cerere", function(req, res){
+    res.send("<p style='color:blue'>Buna ziua</p>")
+})
+
+
+app.get("/fisier", function(req, res, next){
+    res.sendfile(path.join(__dirname,"package.json"));
+})
+
+
+app.get("/*.ejs", function(req, res, next){
+    afisareEroare(res,400);
+})
+
+
+app.get("/*", function(req, res, next){
+    try{
+        res.render("pagini"+req.url,function (err, rezultatRandare){
+            if (err){
+                if(err.message.startsWith("Failed to lookup view")){
+                    afisareEroare(res,404);
+                }
+                else{
+                    afisareEroare(res);
+                }
+            }
+            else{
+                console.log(rezultatRandare);
+                res.send(rezultatRandare)
+            }
+        });
+    }
+    catch(errRandare){
+        if(errRandare.message.startsWith("Cannot find module")){
+            afisareEroare(res,404);
+        }
+        else{
+            afisareEroare(res);
+        }
+    }
+})
+
+
+const vectFoldere = ["temp", "temp1"];
+vectFoldere.forEach(folder => {
+    const caleFolder = path.join(__dirname, folder);
+    if (!fs.existsSync(caleFolder)) {
+        fs.mkdirSync(caleFolder);
+        console.log("Folderul a fost creat.");
+    } else {
+        console.log("Folderul există deja.");
+    }
+});
+
+
+app.listen(8080);
+console.log("Serverul a pornit")
